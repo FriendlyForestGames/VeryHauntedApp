009224be62c35a2b686f2bd18ca9b65118994ac9
connor <connorsmyth@rogers.com>
2024-03-21 16:41:38 -0400

    WIP on client

--- app/api/nakama/nakama-client.js
+++ app/api/nakama/nakama-client.js
@@ -1,5 +1,19 @@
 import { Client } from '@heroiclabs/nakama-js';
 
-const useSSL = false; // Enable if server is run with an SSL certificate.
-// TODO: this should use .env file variables
-const client = new Client('defaultkey', '127.0.0.1', '7350', useSSL);
+export class NakamaClient {
+  client;
+
+  constructor() {
+    const useSSL = false; // Enable if server is run with an SSL certificate.
+    // TODO: this should use .env file variables
+    this.client = new Client('defaultkey', '127.0.0.1', '7350', useSSL);
+  }
+
+  /**
+   * TODO: this is a temp method to validate unit testing
+   */
+  authenticateEmail = async (email, password) => {
+    const session = await this.client.authenticateEmail(email, password);
+    console.info(session);
+  };
+}
