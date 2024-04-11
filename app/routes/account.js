1539e14d03441e53b0fd0c0fe94ec8cb5f09c02d
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-10 23:25:53 -0400

    get method on router

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,2 +1,5 @@
 const express = require("express");
 const router = express.Router();
+
+// using get method to retrieve an account by a specified UUID
+router.get("/accounts/:accountUuid", (req, res));
