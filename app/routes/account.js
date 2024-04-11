e3b5880d4554746a5321f3b34dbcd5f35628c826
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-10 23:31:37 -0400

    pulling params, sending message, comments

--- app/routes/account.js
+++ app/routes/account.js
@@ -2,4 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 // using get method to retrieve an account by a specified UUID
-router.get("/accounts/:accountUuid", (req, res));
+router.get("/accounts/:accountUuid", (req, res) => {
+  //using req to pull parameters from Uuid
+  const accountUuid = req.params.accountUuid;
+  //using template literal to send message with Uuid interpolated
+  res.send(`Retrieving account with UUID: ${accountUuid}`);
+});
