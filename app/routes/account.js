972353ef42aa085e6bc4e8a2531c066f662c3fea
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-12 12:05:20 -0400

    Update app/routes/account.js
    Co-authored-by: Connor Smyth <connorsmyth@rogers.com>

--- app/routes/account.js
+++ app/routes/account.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // using get method to retrieve an account by a specified UUID
-router.get("/accounts/:accountUuid", (req, res) => {
+router.get("/:accountUuid", (req, res) => {
   //using req to pull parameters from Uuid
   const accountUuid = req.params.accountUuid;
   //using template literals to send message with Uuid interpolated
