2749d8ec774b47297da8380d1d23968f155b67c4
connor <connorsmyth@rogers.com>
2024-04-13 15:28:32 -0400

    Fix

--- app/test/unit/api/nakama/nakama.test.js
+++ app/test/unit/api/nakama/nakama.test.js
@@ -1,7 +1,7 @@
 const nakama = require('../../../../api/nakama/nakama');
 const { Client } = require('@heroiclabs/nakama-js');
 
-describe('nakama tests', () => {
+describe('Nakama singleton tests', () => {
   test('It gets client instance', () => {
     expect(nakama.getInstance().client).toBeInstanceOf(Client);
   });
