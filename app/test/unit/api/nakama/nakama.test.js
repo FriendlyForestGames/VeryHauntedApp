6bf6f0713a39ad8248d61d0181301b8aff88d3be
connor <connorsmyth@rogers.com>
2024-04-13 15:05:26 -0400

    Working on tests

---
+++ app/test/unit/api/nakama/nakama.test.js
@@ -0,0 +1,9 @@
+const { describe, expect, test } = require('jest');
+const nakama = require('../../../../api/nakama/nakama');
+const { Client } = require('@heroiclabs/nakama-js');
+
+describe('nakama tests', () => {
+  test('It gets client instance', () => {
+    expect(nakama.getInstance().client).toBeInstanceOf(Client);
+  });
+});
