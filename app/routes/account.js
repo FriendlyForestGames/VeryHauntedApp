68dfb577a74c1f06e66714e348af1316907fe63a
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-22 22:34:40 -0400

    WIP

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const {
+  readAccountByUuid,
+} = require("../controllers/accounts/read-account-by-uuid-controller");
 
 // using get method to retrieve an account by a specified UUID
-router.get("/:accountUuid", (req, res) => {
-  //using req to pull parameters from Uuid
-  const accountUuid = req.params.accountUuid;
-  //using template literals to send message with Uuid interpolated
-  res.send(`Retrieving account with UUID: ${accountUuid}`);
-});
+router.get("/:accountUuid", readAccountByUuid);
 
 //export the router!
 module.exports = router;
