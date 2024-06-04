088a40e0c8c2bc7f0c851ee8575b261786708f2f
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-03 22:33:02 -0400

    WIP

--- app/controllers/accounts/read-account-by-uuid-controller.js
+++ app/controllers/accounts/read-account-by-uuid-controller.js
@@ -1,1 +1,8 @@
 const { validateUuid } = require("app/validation/util/uuid-validator.js");
+
+const readAccountByUuid = (req, res) => {
+  const { uuid } = req.params;
+
+  if (!validateUuid(uuid)) {
+  }
+};
