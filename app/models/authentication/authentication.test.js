bd8dfb5d039ce1743431496e0c7f46a745ca9d4b
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-10 11:39:08 -0400

    wip

--- app/models/authentication/authentication.test.js
+++ app/models/authentication/authentication.test.js
@@ -3,4 +3,13 @@ const nakama = require("../../api/nakama/nakama");
 
 describe("Authentication", () => {
   let authentication;
+
+  beforeEach(() => {
+    authentication = new Authentication();
+  });
+
+  test("authenticateEmail returns a Promise<ApiSession>", async () => {
+    const email = "sample@email.com";
+    const password = "password123";
+  });
 });
