6edba896aef71ac51736e8e4bf406ffaa8179b44
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-06-10 15:56:53 -0400

    5 create controller to handle incoming account requests (#7)
    * file path created
    
    * WIP
    
    * Update app/controllers/accounts/read-account-by-uuid-controller.js
    
    Co-authored-by: Connor Smyth <connorsmyth@rogers.com>
    
    ---------
    
    Co-authored-by: Connor Smyth <connorsmyth@rogers.com>

---
+++ app/controllers/accounts/read-account-by-uuid-controller.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const readAccountByUuid = router.get("/:accountUuid", (req, res) => {
+  //using req to pull parameters from Uuid
+  const accountUuid = req.params.accountUuid;
+  //using template literals to send message with Uuid interpolated
+  res.send(`Retrieving account with UUID: ${accountUuid}`);
+});
+
+module.exports = {
+  readAccountByUuid,
+};
